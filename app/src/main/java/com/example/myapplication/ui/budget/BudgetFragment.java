0229aacfd01a484e526b452517370f14fdd3e398
Benjamin Karstad <benkarstad@gmail.com>
2020-10-02 15:31:42 -0600

    add currency converter to UI;

--- app/src/main/java/com/example/myapplication/ui/budget/BudgetFragment.java
+++ app/src/main/java/com/example/myapplication/ui/budget/BudgetFragment.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.R;
@@ -20,16 +19,26 @@ public class BudgetFragment extends Fragment {
 
 	public View onCreateView(@NonNull LayoutInflater inflater,
 							 ViewGroup container, Bundle savedInstanceState) {
-		budgetViewModel =
-				ViewModelProviders.of(this).get(BudgetViewModel.class);
+		budgetViewModel = ViewModelProviders.of(this).get(BudgetViewModel.class);
 		View root = inflater.inflate(R.layout.fragment_budget, container, false);
-		final TextView textView = root.findViewById(R.id.text_budget);
-		budgetViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-			@Override
-			public void onChanged(@Nullable String s) {
-				textView.setText(s);
-			}
-		});
+
+		Spinner[] spinners = {
+				root.findViewById(R.id.fromCurrency),
+				root.findViewById(R.id.toCurrency)
+		};
+
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this.getContext(),
+				R.array.currencies,
+				android.R.layout.simple_spinner_item);
+
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		// Apply the adapter to the spinners
+		for(Spinner spinner : spinners) spinner.setAdapter(adapter);
+
 		return root;
 	}
 }
--- app/src/main/java/com/example/myapplication/ui/budget/BudgetViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/budget/BudgetViewModel.java
@@ -1,19 +1,7 @@
 package com.example.myapplication.ui.budget;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class BudgetViewModel extends ViewModel {
-
-	private MutableLiveData<String> mText;
-
-	public BudgetViewModel() {
-		mText = new MutableLiveData<>();
-		mText.setValue("This is budget fragment");
-	}
-
-	public LiveData<String> getText() {
-		return mText;
-	}
+	public BudgetViewModel() {}
 }
