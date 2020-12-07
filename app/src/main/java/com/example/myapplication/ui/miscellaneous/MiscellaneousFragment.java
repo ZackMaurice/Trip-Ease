e1d1ba990225e2004cd987e6717f493fff6374b7
Gramaru <70973634+Gramaru@users.noreply.github.com>
2020-12-07 12:24:28 -0500

    Misc Fragment - Password Change / Email Confirmation - Toast messages for buttons - Link dependant upon Guest/User

--- app/src/main/java/com/example/myapplication/ui/miscellaneous/MiscellaneousFragment.java
+++ app/src/main/java/com/example/myapplication/ui/miscellaneous/MiscellaneousFragment.java
@@ -6,21 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.ui.budget.BudgetViewModel;
 import com.example.myapplication.ui.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.example.myapplication.MainActivity;
-import com.example.myapplication.R;
+
+
+
 public class MiscellaneousFragment extends Fragment {
 
     private MiscellaneousViewModel miscellaneousViewModel;
@@ -30,18 +30,36 @@ public class MiscellaneousFragment extends Fragment {
         miscellaneousViewModel = ViewModelProviders.of(this).get(MiscellaneousViewModel.class);
         View root = inflater.inflate(R.layout.fragment_miscellaneous, container, false);
 
-        Button loginBtn   = root.findViewById(R.id.loginButton);
-        Button logoutBtn  = root.findViewById(R.id.logoutButton);
+        Button loginBtn      = root.findViewById(R.id.loginButton);
+        Button logoutBtn     = root.findViewById(R.id.logoutButton);
+        Button passwordBtn   = root.findViewById(R.id.passwordChange);
+        Button emailBtn      = root.findViewById(R.id.verifyEmail);
+        Button confirmBtn    = root.findViewById(R.id.confirmChange);
+
+        EditText confirmEmail     = root.findViewById(R.id.confirmEmail);
+        EditText currentPassword  = root.findViewById(R.id.confirmPassword);
+        EditText newPassword      = root.findViewById(R.id.newPassword);
 
         loginBtn.setVisibility(View.GONE);
         logoutBtn.setVisibility(View.GONE);
-        if(FirebaseAuth.getInstance().getCurrentUser() != null)
+        passwordBtn.setVisibility(View.GONE);
+        emailBtn.setVisibility(View.GONE);
+        confirmBtn.setVisibility(View.GONE);
+
+        confirmEmail.setVisibility(View.GONE);
+        currentPassword.setVisibility(View.GONE);
+        newPassword.setVisibility(View.GONE);
+
+        if(FirebaseAuth.getInstance().getCurrentUser().isAnonymous())
         {
-            logoutBtn.setVisibility(View.VISIBLE);
-        }
-        else{
             loginBtn.setVisibility(View.VISIBLE);
         }
+        else{
+            logoutBtn.setVisibility(View.VISIBLE);
+            passwordBtn.setVisibility(View.VISIBLE);
+            emailBtn.setVisibility(View.VISIBLE);
+        }
+
         loginBtn.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
@@ -52,6 +70,21 @@ public class MiscellaneousFragment extends Fragment {
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
         });
+
+        passwordBtn.setOnClickListener(v -> {
+            confirmEmail.setVisibility(View.VISIBLE);
+            currentPassword.setVisibility(View.VISIBLE);
+            newPassword.setVisibility(View.VISIBLE);
+            confirmBtn.setVisibility(View.VISIBLE);
+        });
+
+        confirmBtn.setOnClickListener((v -> {
+            Toast.makeText(getContext(), "Password Updated!", Toast.LENGTH_SHORT).show();
+        }));
+
+        emailBtn.setOnClickListener((v -> {
+            Toast.makeText(getContext(), "Email Link Sent!", Toast.LENGTH_SHORT).show();
+        }));
         return root;
     }
 }
